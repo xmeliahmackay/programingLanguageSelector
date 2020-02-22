9480fbd862b09067a0051056407fd1c6d29f4b48
Meliah MacKay <xmeilahmackay@gmail.com>
2020-02-22 13:54:11 -0800

    add if and else to jquery to determine what answer user gets

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,18 @@ $(document).ready(function(){
     var qThree = parseInt($("#vacay").val());
     var qFour = parseInt($("#race").val());
     var qFive = parseInt($("#jb").val());
-    alert(qOne + qTwo + qThree + qFive);
     var score = qOne + qTwo + qThree + qFour + qFive; 
-    alert(score);
+    //alert(score);
+
+    if (score >= 5 && score <= 10) {
+      $("#ruby").show();
+    } else if (score >= 10 && score <= 17) {
+      $("#cSharp").show();
+    } else if (score >= 17) {
+      $("#python").show();
+    }
+    
+
+    event.preventDefault();
   });
-
-
-
-
 });
